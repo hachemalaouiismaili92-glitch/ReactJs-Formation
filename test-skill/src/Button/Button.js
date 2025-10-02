7194e615d9cd626ba25f01b52058648a249bb352
hachemalaouiismaili92-glitch <hachem.alaouiismaili92@gmail.com>
2025-10-02 14:41:05 +0100

    step4

--- test-skill/src/Button/Button.js
+++ test-skill/src/Button/Button.js
@@ -1,10 +1,17 @@
 import './Button.css'
+import { useState } from 'react';
 
 
 function Botona() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <button className="btn">
-      I'm a buttona
+    <button onClick={handleClick} className="btn">
+      Clicked {count} times
     </button>
   );
 }
