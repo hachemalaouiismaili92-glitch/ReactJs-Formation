907375207d6acdf12df0d719f32fc320df5d0acd
hachemalaouiismaili92-glitch <hachem.alaouiismaili92@gmail.com>
2025-10-02 14:24:18 +0100

    step 2

--- test-skill/src/App.js
+++ test-skill/src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Botona from './Button';
 
-function App() {
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my app</h1>
+      <Botona />
     </div>
   );
 }
 
-export default App;
+
+
+
---
+++ test-skill/src/Button.js
@@ -0,0 +1,12 @@
+import './Button.css'
+
+
+function Botona() {
+  return (
+    <button className="btn">
+      I'm a buttona
+    </button>
+  );
+}
+
+export default Botona;
